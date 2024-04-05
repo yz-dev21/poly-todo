d52d9d62fcb80077d5eceef80df1fe69b2314a1a
Bugger525 <yunjae00734@gmail.com>
2024-04-05 23:44:22 +0900

    i fucking hate css i woke up this morning to remember that css is fucking sucks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Sidebar from "./Sidebar.svelte";
+</script>
+
+<div class="container-fluid">
+    <div class="row flex-nowrap">
+        <div class="col-auto col-sm-1 px-0" id="indigo_subtle">
+            <div class="d-flex flex-column flex-shrink-0" id="sidebar_parent">
+                <Sidebar />
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    :global(body) {
+        background-color: black;
+    }
+    #indigo_subtle {
+        color: #6814f4;
+    }
+    #sidebar_parent {
+        width: 3.5rem;
+    }
+</style>
---
+++ src/routes/Sidebar.svelte
@@ -0,0 +1,31 @@
+<ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+	<li class="nav-item">
+		<a href="/" class="nav-link align-middle py-3 px-0 link-light">
+			<i class="bi bi-check2-square"></i>
+		</a>
+	</li>
+	<li class="nav-item">
+		<a href="/" class="nav-link align-middle py-3 px-0 link-light">
+			<i class="bi bi-bounding-box"></i>
+		</a>
+	</li>
+	<li class="nav-item">
+		<a href="/" class="nav-link align-middle py-3 px-0 link-light">
+			<i class="bi bi-gear-wide-connected"></i>
+		</a>
+	</li>
+</ul>
+<div class="d-flex">
+	<a href="/" class="nav-link align-middle link-light w-100">
+		<i class="bi bi-github"></i>
+	</a>
+</div>
+
+<style>
+	i {
+		font-size: 150%;
+	}
+	a:hover {
+		background-color: #6814f4;
+	}
+</style>
