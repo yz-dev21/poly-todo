ae89339353d62eb04879f7a1c61dbe561a31e411
Bugger525 <yunjae00734@gmail.com>
2024-04-15 00:53:11 +0900

    (docs) Fixed some typos and added comments in TodoList.svelte

--- src/routes/TodoList.svelte
+++ src/routes/TodoList.svelte
@@ -2,44 +2,37 @@
 	import TodoItem from './TodoItem.svelte';
 	import TodoPrompt from './TodoPrompt.svelte';
 
-	// let todoList = [];
-	let todoList = [
-		{
-			id: 1,
-			text: 'Todo #1',
-			done: false
-		},
-		{
-			id: 2,
-			text: 'Todo #2',
-			done: true
-		}
-	];
-	function addTodo(e) {
+	let todoList = [];
+	// todo utility functions
+	function createTodo(pId, pText, pDone, pType) {
+		const newTodo = {
+			id: pId,
+			text: pText,
+			done: pDone,
+			type: pType
+		};
+		return newTodo;
+	}
+	// event handling functions
+	function handleOnAdd(e) {
 		let lastId = 0;
 		if (todoList.length > 0) lastId = todoList[todoList.length - 1].id;
 
-		const newTodo = {
-			id: lastId + 1,
-			text: e.detail.value,
-			done: false
-		};
-
 		// .push(...) returns length of array instead of new array.
-		todoList.push(newTodo);
+		todoList.push(createTodo(lastId + 1, e.detail.value, false, []));
 		// ^ due to this reason, code below is needed for reactivity.
 		todoList = todoList;
 	}
-	function deleteTodo(id) {
+	function handleOnDelete(id) {
 		// .filter(...) returns new array.
 		todoList = todoList.filter((val) => val.id != id);
 	}
 </script>
 
-<TodoPrompt on:add={addTodo} />
+<TodoPrompt on:add={handleOnAdd} />
 
 <ul>
 	{#each todoList as todo}
-		<TodoItem {todo} on:delete={deleteTodo(todo.id)} />
+		<TodoItem {todo} on:delete={handleOnDelete(todo.id)} />
 	{/each}
 </ul>
